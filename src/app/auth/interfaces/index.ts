dcd90ba2dc71dc1b1bec684edc9763ff9cff6d3a
David Muñoz <dmp.david@gmail.com>
2023-12-11 23:50:58 +0100

    fin sección 26

---
+++ src/app/auth/interfaces/index.ts
@@ -0,0 +1,5 @@
+
+export * from './auth-status.enum';
+export * from './check-token.response';
+export * from './login-response.interface';
+export * from './user.interface';
---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Component({
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent {
+
+  private fb          = inject( FormBuilder );
+  private authService = inject( AuthService );
+  private router      = inject( Router )
+
+  public myForm: FormGroup = this.fb.group({
+    email:    ['david@google.com', [ Validators.required, Validators.email ]],
+    password: ['123456', [ Validators.required, Validators.minLength(6) ]],
+  });
+
+  login() {
+    // console.log(this.myForm.value);
+
+    const { email, password } = this.myForm.value;
+
+    this.authService.login(email, password)
+      .subscribe({
+        // console.log({success});
+        next: () => this.router.navigateByUrl('/dashboard'),
+        error: (message) => {
+          Swal.fire('Error', message, 'error' )
+        }
+      })
+
+  }
+
+}
